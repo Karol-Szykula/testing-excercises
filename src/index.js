fe8c4f897e6acb32c1f247f18a79b68256377bd7
Karol szykula <karol.szykula@gmail.com>
2018-12-10 12:01:02 +0100

    Add shipping price tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 export const orderTotal = (arrayOfOrders) => (
     arrayOfOrders.reduce((reduced, item) => {
         if (item.quantity || item.quantity === 0) { reduced = reduced + (item.price * item.quantity) }
-        else if (!item.quantity) { reduced = reduced + item.price }
+        else if (!item.quantity && !item.shipping) { reduced = reduced + item.price }
+        if (item.shipping && item.freeShipping >= reduced) { reduced += item.price }
         return reduced
     }, 0)
 )
