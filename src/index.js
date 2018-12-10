ef9c22ae80a84b654bfba387a8da3a8cfbeb4e4d
Karol szykula <karol.szykula@gmail.com>
2018-12-10 11:24:24 +0100

    Add test case with quantity of items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export const orderTotal = (arrayOfOrders) => (
-    arrayOfOrders.reduce((reduced, item) => reduced += item.price, 0)
+    arrayOfOrders.reduce((reduced, item) => {
+        if (item.quantity) { reduced = reduced + (item.price * item.quantity) }
+        else if (!item.quantity) { reduced = reduced + item.price }
+        return reduced
+    }, 0)
 )
