2f6036399f2030799e70d87a69c3a3732760e767
Karol szykula <karol.szykula@gmail.com>
2018-12-10 11:49:46 +0100

    Add describe functions

--- src/index.test.js
+++ src/index.test.js
@@ -1,54 +1,58 @@
 import { orderTotal } from './index'
 
-const orders = [
-    {
-        name: 'Lager',
-        price: 10
-    },
-    {
-        name: 'APA',
-        price: 15
-    }
-]
+describe('Sum without quantity', () => {
+    const orders = [
+        {
+            name: 'Lager',
+            price: 10
+        },
+        {
+            name: 'APA',
+            price: 15
+        }
+    ]
 
-test('Happy path failed', () => {
-    expect(
-        orderTotal(orders)
-    ).toBe(25)
+    test('Happy path failed', () => {
+        expect(
+            orderTotal(orders)
+        ).toBe(25)
+    })
+
+    test('Empty order list must equals 0', () => {
+        expect(
+            orderTotal([])
+        ).toBe(0)
+    })
 })
 
-test('Empty order list must equals 0', () => {
-    expect(
-        orderTotal([])
-    ).toBe(0)
-})
+describe('Sum with quantity', () => {
+    const ordersWithQuantity = [
+        {
+            name: 'Lager',
+            price: 10,
+            quantity: 2,
+        },
+        {
+            name: 'APA',
+            price: 15,
+            quantity: 1
+        }
+    ]
 
-const ordersWithQuantity = [
-    {
-        name: 'Lager',
-        price: 10,
-        quantity: 2,
-    },
-    {
-        name: 'APA',
-        price: 15,
-        quantity: 1
-    }
-]
+    test('Fails to calculate sum with quantity', () => {
+        expect(
+            orderTotal(ordersWithQuantity)
+        ).toBe(35)
+    })
 
-test('Fails to calculate sum with quantity', () => {
-    expect(
-        orderTotal(ordersWithQuantity)
-    ).toBe(35)
-})
-
-test('Fails to calculate sum with quantity equal 0', () => {
-    expect(
-        orderTotal([
-            {
-                name: 'Lager',
-                price: 10,
-                quantity: 0,
-            }])
-    ).toBe(0)
+    test('Fails to calculate sum with quantity equal 0', () => {
+        expect(
+            orderTotal([
+                {
+                    name: 'Lager',
+                    price: 10,
+                    quantity: 0,
+                }])
+        ).toBe(0)
+    })
 })
