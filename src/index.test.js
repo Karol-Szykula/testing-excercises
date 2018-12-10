77483c2bb03431aec0790170487660071a864482
Karol szykula <karol.szykula@gmail.com>
2018-12-10 11:11:29 +0100

    Add new test

--- src/index.test.js
+++ src/index.test.js
@@ -22,3 +22,22 @@ test('Empty order list must equals 0', () => {
         orderTotal([])
     ).toBe(0)
 })
+
+const ordersWithQuantity = [
+    {
+        name: 'Lager',
+        price: 10,
+        quantity: 2,
+    },
+    {
+        name: 'APA',
+        price: 15,
+        quantity: 1
+    }
+]
+
+test('Fails to calculate sum with quantity', () => {
+    expect(
+        orderTotal(ordersWithQuantity)
+    ).toBe(35)
+})
