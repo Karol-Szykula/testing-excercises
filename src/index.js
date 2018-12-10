c2604b933ee19fe9bb575796b6bfbe98cb5d6255
Karol szykula <karol.szykula@gmail.com>
2018-12-10 10:09:16 +0100

    Add items and addition function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+const orders = [
+    {
+        name: 'Lager',
+        price: 10
+    },
+    {
+        name: 'APA',
+        price: 15
+    }
+]
 
-ReactDOM.render(
-    '<App />',
-    document.getElementById('root')
-)
+const orderTotal = (arrayOfOrders) => {
+
+    return arrayOfOrders.reduce((reduced, item, index, array) => {
+        return reduced = reduced + item.price
+    }, 0)
+}
+
+console.log(orderTotal(orders))
